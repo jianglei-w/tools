0d90f23fb13387af133209c8b6bf1a13c5940778
JL <jianglei-w@qq.com>
2023-05-23 21:03:48 +0800

    first commit

---
+++ pull_and_push.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+# This script requires a positional parameter, which is the image name
+
+# 是否为空
+if [ -z ${1} ]; then 
+    echo "err!!! position parameter is empty!!!"
+fi
+
+# 长度是否小于6
+if [ $(expr length "$1") -lt 6 ]; then
+    echo "This doesn't look like a image name, because it to short!"   
+fi
+
+
+push_tag=jiangleiww/`echo ${1} | awk -F / '{print $NF}'`
+
+function start {
+    # echo "start.."
+
+    # echo ${1}
+
+    docker pull ${1}
+
+
+    docker tag ${1} ${push_tag}
+    
+    docker login
+
+    docker push ${push_tag}
+}
+
+# docker pull $1
+
+start ${1}
